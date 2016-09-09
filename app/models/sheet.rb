1ac62bbf61f79374a4aaf9a6ec038aeed0c1ed08
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-09-09 12:02:25 +0200

    Add relation between sheets and tags

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,2 +1,3 @@
 class Sheet < ApplicationRecord
+  has_and_belongs_to_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909095244) do
+ActiveRecord::Schema.define(version: 20160909100114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20160909095244) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sheets_tags", id: false, force: :cascade do |t|
+    t.integer "sheet_id", null: false
+    t.integer "tag_id",   null: false
   end
 
   create_table "tags", force: :cascade do |t|
