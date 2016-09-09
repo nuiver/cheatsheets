7978636abdd5401d75549578984a4a2d8ea660f8
Mark Nuiver <mark@vrijblikken.nl>
2016-09-09 13:22:14 +0200

    Migration changing names

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909105407) do
+ActiveRecord::Schema.define(version: 20160909111636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
     t.string   "avatar"
     t.text     "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "last_name"
   end
 
   create_table "sheets", force: :cascade do |t|
