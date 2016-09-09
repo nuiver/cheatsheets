74d61b4b31ca1826bca4d1cc17ec2567959db73a
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-09-09 12:52:26 +0200

    Seeds with sheets and tags

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,24 @@ ActiveRecord::Schema.define(version: 20160909101417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "sheets", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sheets_tags", id: false, force: :cascade do |t|
+    t.integer "sheet_id", null: false
+    t.integer "tag_id",   null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -30,24 +48,6 @@ ActiveRecord::Schema.define(version: 20160909101417) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "sheets", force: :cascade do |t|
-    t.string   "title"  
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sheets_tags", id: false, force: :cascade do |t|
-    t.integer "sheet_id", null: false
-    t.integer "tag_id",   null: false
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
