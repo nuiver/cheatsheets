f16e4047c9c9938b4f79102f47d7cf51bcd7074f
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-09-10 12:03:18 +0200

    add sort on created_at in middle column

--- app/helpers/sheets_helper.rb
+++ app/helpers/sheets_helper.rb
@@ -1,2 +1,13 @@
 module SheetsHelper
+  def sort_link(column, title = nil, tag = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"
+    icon = column == sort_column ? icon : ""
+    if !tag.nil?
+      link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction, tag: tag[:tag]}
+    else
+      link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+    end
+  end
 end
